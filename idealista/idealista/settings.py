383d0b1d4fa05cde207c9f48abbd07b068971f12
Iván Canales Martin <ivancanalestgna@gmail.com>
2019-10-08 15:46:48 +0200

    Fixed login form template

--- idealista/idealista/settings.py
+++ idealista/idealista/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-   'idealista_app.apps.IdealistaAppConfig'
-
+   'idealista_app.apps.IdealistaAppConfig',
+   'widget_tweaks'
 ]
 
 
