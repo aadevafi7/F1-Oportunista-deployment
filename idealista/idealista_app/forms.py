ddd58d5aa8f4bc831c1338efb00ac8cfabab68b8
kanales <ivancanalestgna@gmail.com>
2019-10-08 19:37:52 +0200

    Fixed email->username passwrd->password
    Added remember field (checkbox)

--- idealista/idealista_app/forms.py
+++ idealista/idealista_app/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
-class LoginForm(forms.Form):
-    email = forms.EmailField(label='Email de acceso')
-    passwd = forms.CharField(widget=forms.PasswordInput)
 
+class LoginForm(forms.Form):
+    username = forms.EmailField(label='Email de acceso')
+    password = forms.CharField(
+        widget=forms.PasswordInput, label='Tu contraseña')
+    remember = forms.BooleanField(label='Recordar tus datos')
--- idealista/idealista_app/urls.py
+++ idealista/idealista_app/urls.py
@@ -3,13 +3,11 @@ from . import views
 from idealista_app.views import register_user
 
 urlpatterns =[
-
     url(
         regex=r'^register/$',
         view=views.register_user,
         name='register_user'
     ),
-
     url(
         regex=r'^home/$',
         view=views.homePage,
@@ -19,6 +17,7 @@ urlpatterns =[
         regex=r'^submit/$',
         view=views.submit,
         name='submit'
+    ),
     url(
         regex=r'^login/',
         view=views.login,
