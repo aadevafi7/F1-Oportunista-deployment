c07d8521f3b3128760244c236d9bb2c192dd2015
Iván Canales Martin <ivancanalestgna@gmail.com>
2019-10-08 16:10:27 +0200

    Fixed loading of static data

--- idealista/idealista/settings.py
+++ idealista/idealista/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, '..', 'static'),
+)
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR, 'static'),
