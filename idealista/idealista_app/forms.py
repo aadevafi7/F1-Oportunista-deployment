1d02b053d3fb9425598354a79c9e7510231c600e
kanales <ivancanalestgna@gmail.com>
2019-10-09 11:19:48 +0200

    Adapted template to use form

--- idealista/idealista_app/forms.py
+++ idealista/idealista_app/forms.py
@@ -1,4 +1,7 @@
+
 from django import forms
+
+from .dummies import add_user, user_exists
 
 
 class LoginForm(forms.Form):
@@ -6,3 +9,30 @@ class LoginForm(forms.Form):
     password = forms.CharField(
         widget=forms.PasswordInput, label='Tu contraseña')
     remember = forms.BooleanField(label='Recordar tus datos')
+
+
+class RegisterForm(forms.Form):
+    email = forms.EmailField(label='Email de acceso')
+    name = forms.CharField(label='Nombre')
+    password = forms.CharField(
+        widget=forms.PasswordInput, label='Tu contraseña')
+    aceptar_privacidad = forms.BooleanField(
+        label='Aceptar la política de privacidad')
+    recibir_info = forms.BooleanField(label="""
+        Recibir información de inmuebles,
+        noticias y otras comunicaciones
+        promocionales desde idealista,
+        idealista/data, idealista/hipotecas o
+        Rentalia basadas en tu perfil.""")
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super(RegisterForm, self).__init__(*args, **kwargs)
+
+    def clean_email_address(self):
+        email = self.cleaned_data.get('email_address')
+        if self.user and self.user.email == email:
+            return email
+        if user_exists(email):
+            raise forms.ValidationError(u'That email address already exists.')
+        return email
--- idealista/idealista_app/views.py
+++ idealista/idealista_app/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
+
+from .dummies import add_user
 # Create your views here.
 
 
 def register_user(request):
-    return render(request, 'idealista_app/register.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('name')
+            email = form.clean_email_address()
+            raw_password = form.cleaned_data.get('password')
+            add_user(username, email, raw_password)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+        return render(request, 'idealista_app/register.html', {'form': form})
+
 
 def homePage(request):
     return render(request, 'idealista_app/home.html')
 
+
 def submit(request):
     return render(request, 'idealista_app/submit.html')
+
 
 def login(request):
     if request.method == 'POST':
@@ -21,4 +38,4 @@ def login(request):
             return HttpResponseRedirect('/')
     else:
         form = LoginForm()
-        return render(request, 'idealista_app/login.html', { 'form': form })
+        return render(request, 'idealista_app/login.html', {'form': form})
