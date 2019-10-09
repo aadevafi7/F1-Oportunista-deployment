f3e3211454c18b4c711048e7a6696bf2a0a009ab
kanales <ivancanalestgna@gmail.com>
2019-10-09 12:39:11 +0200

    urls were removed

---
+++ idealista/idealista_app/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(
+        regex=r'^register/$',
+        view=views.register_user,
+        name='register_user'
+    ),
+    url(
+        regex=r'^home/$',
+        view=views.homePage,
+        name='homePage'
+    ),
+    url(
+        regex=r'^submit/$',
+        view=views.submit,
+        name='submit'
+    ),
+    url(
+        regex=r'^login/',
+        view=views.login,
+        name='login'
+    )
+]
