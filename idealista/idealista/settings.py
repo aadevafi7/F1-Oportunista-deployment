17e6f6bcd1eafd797a2538636192390e047f2e27
kanales <ivancanalestgna@gmail.com>
2019-10-09 11:20:19 +0200

    cleanup in settings

--- idealista/idealista/settings.py
+++ idealista/idealista/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-   'idealista_app.apps.IdealistaAppConfig',
-   'widget_tweaks'
+    'idealista_app.apps.IdealistaAppConfig',
+    'widget_tweaks'
 ]
 
 
@@ -118,14 +118,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Templates
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates/'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '..', 'static'),
-)
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
