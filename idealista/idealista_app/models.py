9a0b895746850c1c7c93e02fdce869d9a06d5fe5
aadevafi7 <aadevafi7@alumnes.ub.edu>
2019-11-25 18:41:46 +0100

    Modificado modelo property

--- idealista/idealista_app/models.py
+++ idealista/idealista_app/models.py
@@ -29,14 +29,22 @@ class property(models.Model):
     op_type = models.IntegerField
     description = models.TextField(max_length=500)
     address = models.CharField(max_length=150)
+    address_number = models.CharField(max_length=5)
     floor = models.CharField(max_length=15)
     door = models.CharField(max_length=15)
-    rooms = models.IntegerField
-    bath = models.IntegerField
+    m_built = models.DecimalField(max_digits=15, decimal_places=2)
+    m_use = models.DecimalField(max_digits=15, decimal_places=2)
+    rooms = models.IntegerField()
+    bath = models.IntegerField()
+    is_exterior = models.SmallIntegerField()
+    has_elevator = models.SmallIntegerField()
+    price = models.DecimalField(max_digits=15, decimal_places=2)
     city = models.ForeignKey(location, on_delete=models.DO_NOTHING)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=9)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name + ' (' + self.city + ')'
