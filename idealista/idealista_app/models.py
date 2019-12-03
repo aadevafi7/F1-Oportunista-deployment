e65b4e81c06d792c8b85c304d567fa9ee1100ed5
aadevafi7 <aadevafi7@alumnes.ub.edu>
2019-12-03 18:36:48 +0100

    Declarado test view posts propios

--- idealista/idealista_app/models.py
+++ idealista/idealista_app/models.py
@@ -11,24 +11,7 @@ Modelos / tablas de la aplicación Oportunista
 class PropertyType(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, unique=True)
-    description = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-class State(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Province(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, unique=True)
-    state = models.ForeignKey(State, on_delete=models.DO_NOTHING)
+    description = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -37,28 +20,24 @@ class Province(models.Model):
 class Location(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
-    province = models.ForeignKey(Province, on_delete=models.DO_NOTHING)
-
-    def __str__(self):
-        return self.name + '(' + self.province.name + ')'
 
 
 class Property(models.Model):
     id = models.AutoField(primary_key=True)
     pro_type = models.ForeignKey(PropertyType, on_delete=models.DO_NOTHING)
     name = models.CharField(max_length=50)
-    op_type = models.IntegerField
+    op_type = models.IntegerField(blank=True, null=True)
     description = models.TextField(max_length=500)
     address = models.CharField(max_length=150)
-    address_number = models.CharField(max_length=5)
-    floor = models.CharField(max_length=15)
-    door = models.CharField(max_length=15)
-    m_built = models.DecimalField(max_digits=15, decimal_places=2)
-    m_use = models.DecimalField(max_digits=15, decimal_places=2)
-    rooms = models.IntegerField()
-    bath = models.IntegerField()
-    is_exterior = models.SmallIntegerField()
-    has_elevator = models.SmallIntegerField()
+    address_number = models.CharField(max_length=5, blank=True, null=True)
+    floor = models.CharField(max_length=15, blank=True, null=True)
+    door = models.CharField(max_length=15, blank=True, null=True)
+    m_built = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
+    m_use = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
+    rooms = models.IntegerField(blank=True, null=True)
+    bath = models.IntegerField(blank=True, null=True)
+    is_exterior = models.SmallIntegerField(blank=True, null=True)
+    has_elevator = models.SmallIntegerField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2)
     city = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
     email = models.CharField(max_length=100)
@@ -68,7 +47,7 @@ class Property(models.Model):
     modified_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name + ' (' + self.city.name + ')'
+        return self.name + ' (' + self.city + ')'
 
 
 class PropertyPics(models.Model):
