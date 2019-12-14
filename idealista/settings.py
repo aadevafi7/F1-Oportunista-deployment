b0d7fc44b5f09340bbd6571c595b2f0ddfde084a
aadevafi7 <32069713+aadevafi7@users.noreply.github.com>
2019-12-14 19:56:22 +0100

    Update settings.py

--- idealista/settings.py
+++ idealista/settings.py
@@ -150,8 +150,8 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-#MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
+MEDIA_URL = '/media/'
 
 # Activate Django-Heroku
 django_heroku.settings(locals())
