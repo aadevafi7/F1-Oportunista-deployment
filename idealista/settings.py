7700c693e052e93dde03c72e30651066f51dba35
aadevafi7 <32069713+aadevafi7@users.noreply.github.com>
2019-12-18 15:21:16 +0100

    Update settings.py

--- idealista/settings.py
+++ idealista/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#3vlt16=@#b)dpkk0i))yl*u7xug^rukt3ym8=gzs7_m0!q^6@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
